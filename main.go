a7af66ec43909fa5ae50c92e0c357bfa1973e522
fromxt <fromxt@126.com>
2022-05-08 18:01:10 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "github.com/AaronO/gogo-proxy"
+    "net/http"
+)
+
+func main() {
+    p, _ := proxy.New(proxy.ProxyOptions{
+        Balancer: func(req *http.Request) (string, error) {
+            return "https://www.google.com", nil
+        },
+    })
+    http.ListenAndServe(":"+os.Getenv("PORT"), p)
+    
+}
