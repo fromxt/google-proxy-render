9116cd62203c32fe1b5ae0756478eb7b8a7e08c2
fromxt <fromxt@126.com>
2025-01-23 07:59:00 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://duckduckgo.com", nil
+            return "https://ddg.co", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
