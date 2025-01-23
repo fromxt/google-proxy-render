eac2cb6fac12efdb1cfcb5dc4fe76d341ed023d5
fromxt <fromxt@126.com>
2025-01-23 08:14:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://www.qwant.com", nil
+            return "https://search.brave.com", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
